0335738cac93dc959a7f00c548402e4abe03131a
Nils <Nils@DESKTOP-A75CUSS>
2024-04-28 14:56:25 +0200

    Projektdateien hinzufügen.

---
+++ Playtime counter.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
+
+unsigned long long PlayTime = 0;
+
+bool ShouldCount = true;
+bool Read = true;
+
+const TCHAR* process = TEXT("launcher.exe");
+const std::string FilePath = "PlayTime.txt";
+
+void ReadFile() {
+    if (!Read)
+        return;
+
+    std::ifstream file(FilePath);
+    if (file.is_open()) {
+        file >> PlayTime;
+        file.close();
+        std::cout << "Read PlayTime from file: " << PlayTime << '\n';
+    }
+    else {
+        std::cout << "Unable to open file for reading: " << FilePath << '\n';
+    }
+
+    Read = false;
+}
+
+bool IsProcessRunning(const TCHAR* const executableName) {
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof(PROCESSENTRY32);
+
+    const auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+    if (!Process32First(snapshot, &entry)) {
+        CloseHandle(snapshot);
+        return false;
+    }
+
+    do {
+        if (!_tcsicmp(entry.szExeFile, executableName)) {
+            CloseHandle(snapshot);
+            return true;
+        }
+    } while (Process32Next(snapshot, &entry));
+
+    CloseHandle(snapshot);
+    return false;
+}
+
+void Count() {
+    ULONGLONG lastTick = GetTickCount64();
+
+    while (IsProcessRunning(process)) {
+        ULONGLONG now = GetTickCount64();
+        ULONGLONG duration = now - lastTick;
+        lastTick = now;
+
+        PlayTime += duration / 1000;
+
+        Sleep(1000);
+        std::cout << PlayTime << '\n';
+    }
+}
+
+void WriteFile() {
+    std::ofstream file(FilePath);
+    if (file.is_open()) {
+        file << PlayTime;
+        file.close();
+        std::cout << "Wrote PlayTime to file: " << PlayTime << '\n';
+    }
+    else {
+        std::cout << "Unable to open file for writing: " << FilePath << '\n';
+    }
+}
+
+int main() {
+    ReadFile();
+    Count();
+    WriteFile();
+    return 0;
+}
